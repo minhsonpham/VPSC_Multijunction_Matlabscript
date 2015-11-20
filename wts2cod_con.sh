58ee7399463d1d4c8ab4427bad54921c7b17314e
minhsonpham <minhsonhut@gmail.com>
2015-11-20 13:39:02 -0500

    texture OUT to COD and smoothing

--- wts2cod_con.sh
+++ wts2cod_con.sh
@@ -1,8 +1,9 @@
 #this script is for convert Texture output from vpsc to cod and then do smooth with a width of 7.5
 #!/bin/bash
-for i in 05 10 15
+find . -name "*.ps" | xargs rm
+for i in 5 10 15 20
 do
-/home/minhsonpham/Documents/CMU-NIST/Texture\ analysis/wts2pop/wts2cod TEX_PH1_$i.OUT TEX_PH1_$i.cod 1
-/home/minhsonpham/Documents/CMU-NIST/Texture\ analysis/wts2pop/smoothsod_7.5 TEX_PH1_$i.cod 
-/home/minhsonpham/Documents/CMU-NIST/Texture\ analysis/wts2pop/sodcon TEX_PH1_$i.cmh
+/home/minhsonpham/Documents/Texture\ analysis/wts2pop_subroutine/wts2cod TEX_PH1_$i.OUT TEX_PH1_$i.cod 1
+/home/minhsonpham/Documents/Texture\ analysis/smoothsod_7.5 TEX_PH1_$i.cod 
+/home/minhsonpham/Documents/Texture\ analysis/sodcon_4scale TEX_PH1_$i.cmh
 done
